0fe86a48a3927d4f9ab0170c201b831794b3d100
abrarfuad27 <abrarfuad51@gmail.com>
2023-11-17 23:47:04 -0500

    added gitignore

---
+++ app/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1 className="navbar-header">
+        <span className="navbar-header-C">S</span>
+        ign
+        <span className="navbar-header-M">L</span>
+        ingo
+      </h1>
+    </nav>
+  );
+}
