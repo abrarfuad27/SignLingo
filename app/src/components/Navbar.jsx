333e6b2046d34e7d440ae852df474dced6a909f6
Khyati <cherrykhyati@gmail.com>
2023-11-18 22:17:42 -0500

    navbar redirect to home + quiz 2 data

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
+    <Link to = "/" >
     <nav className="navbar">
       <img className="logo" src='/assets/penguin.png' alt="Logo" />
     </nav>
+    </Link>
     /* <h1 className="navbar-header">
         <span className="navbar-header-C">S</span>
         ign
